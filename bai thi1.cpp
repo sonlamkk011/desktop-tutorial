f9143e9928de5a0356ab2163d98fb79d8e023af0
sonlamkk011 <71081269+sonlamkk011@users.noreply.github.com>
2020-10-06 10:34:33 +0700

    bai thi1

---
+++ bai thi1.cpp
@@ -0,0 +1,35 @@
+
+
+#include <stdio.h>
+#include<math.h>
+float perimeter(int a,int b,int c){
+    return a+b+c;
+}
+float area(int a, int b, int c) {
+    float p = perimeter(a, b, c) / 2.0;
+    return sqrt(p*(p - a)*(p - b)*(p - c));
+}
+int main() {
+    float a,b,c;
+    do{
+        printf("Nhap canh a: ");
+        scanf("%f",&a);
+    }
+    while(a<0);
+    do{
+        printf("Nhap canh b: ");
+        scanf("%f",&b);
+    }
+    while(b<0);
+    do{
+        printf("Nhap canh c: ");
+        scanf("%f",&c);
+    }
+    while(c<0);
+    if(a+b>=c&&b+c>=a&&a+c>=b){
+        printf("\n3 Canh vua nhap tao thanh 1 tam giac");
+        printf("\nDien tich tam giac la:%.2f\nChu vi tam giac la: %.2f\n",area(a,b,c),perimeter(a,b,c));
+        }
+    else{
+    printf("3 canh vua nhap khong la tam giac");
+    }}
